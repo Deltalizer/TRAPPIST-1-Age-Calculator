a7f1fb1856f5e5faf07bcbdf4f733d43913c5bd5
Joseph Dempsey <coolfireking02@gmail.com>
2017-04-24 11:04:30 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.get("*", function(req, res){
+    res.render("404");
+});
+
+app.listen(8005, function(){
+   console.log("TRAPPIST-1 age calculator running..."); 
+});
